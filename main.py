b385d467fd58657183d0d6f44db4315c1364a53e
alexey <atiunchyk.87@gmail.com>
2021-07-09 15:27:35 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+def register2(request):
+    return render(request,
+        'register.html',
+        {}
+    )
+
+def do_logout(request):
+    if request.user.is_authenticated:
+        logout(request)
+        return HttpResponseRedirect('/')
+    else:
+        return HttpResponse('Ты не залогинен')
+
+def index2(request):
+    return render(request,
+        'index2.html',
+        {}
+    )
